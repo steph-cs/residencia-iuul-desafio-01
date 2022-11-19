852c1737f9a81f0f1b0fea74b6ecb8b8c96079d8
stephanie-cs <stecampelo@gmail.com>
2022-11-19 17:21:00 -0300

    js destaque navbar

---
+++ script.js
@@ -0,0 +1,29 @@
+function isInViewport(el) {
+  var rect = el.getBoundingClientRect();
+  var elemTop = rect.top;
+  var elemBottom = rect.bottom;
+
+  // ocupa menos da metade da tela
+  isVisible = elemTop < window.innerHeight/2 && elemBottom > window.innerHeight/2;
+  return isVisible;
+}
+
+const nav = ["home", "perfil", "portfolio", "contato"];
+
+nav.forEach(element => {
+  document.addEventListener('scroll', function () {
+    let el = document.getElementById(element);
+    const messageText = isInViewport(el) ?
+      destacaMenu(element) :
+      "n visivel";
+  }, {
+    passive: true
+  });
+});
+
+function destacaMenu(el){
+  document.getElementsByClassName("active")[0].classList.remove("active");
+  let id = "nav-"+ el
+  let nav = document.getElementById(id)
+  nav.classList.add("active")
+}
